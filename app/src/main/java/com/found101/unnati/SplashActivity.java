2fb2f21d02a093c09aadc3bb99c6ca28b906464e
Vivek <vnjvibhash@gmail.com>
2023-03-17 21:04:34 +0530

    Splash and Main page

--- app/src/main/java/com/found101/unnati/SplashActivity.java
+++ app/src/main/java/com/found101/unnati/SplashActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
-
-    private static int SPLASH_TIME_OUT = 3000;
+    TextView app_name;
+    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT_HINDI = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        app_name = findViewById(R.id.app_name);
 
         new Handler().postDelayed(new Runnable() {
 
@@ -24,13 +28,19 @@ public class SplashActivity extends AppCompatActivity {
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(i);
+                app_name.setText("उन्नति");
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // This method will be executed once the timer is over
+                        // Start your app main activity
+                        Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                        startActivity(i);
 
-                // close this activity
-                finish();
+                        // close this activity
+                        finish();
+                    }
+                }, SPLASH_TIME_OUT_HINDI);
             }
         }, SPLASH_TIME_OUT);
     }
