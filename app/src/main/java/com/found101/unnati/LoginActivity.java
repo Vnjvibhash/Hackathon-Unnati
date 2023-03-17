f0a83e5e9db0ba751ec15bb3053462b5691c7344
Amit Sarkar <amitskr6@gmail.com>
2023-03-17 19:34:29 +0530

    Files added by Amit

---
+++ app/src/main/java/com/found101/unnati/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.found101.unnati;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+
+        Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (username.equals("admin") && password.equals("password")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/found101/unnati/MainActivity.java
@@ -0,0 +1,25 @@
+package com.found101.unnati;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private RecyclerView mPostRecyclerView;
+//    private List<Post> mPosts;
+//    private PostAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mPostRecyclerView = findViewById(R.id.postRecyclerView);
+//        mPosts = new ArrayList<>();
+//        mPosts.add(new Post("Post 1", "Description 1", R.drawable
+    }
+}
