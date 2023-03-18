7516deaf032c9949fd40e2e307d77d34e4ce2eee
Vivek <vnjvibhash@gmail.com>
2023-03-18 05:30:01 +0530

    Login and Register Page Updated

--- app/src/main/java/com/found101/unnati/LoginActivity.java
+++ app/src/main/java/com/found101/unnati/LoginActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    ImageView passwordToggleImageView, passwordImageView;
     private EditText usernameEditText;
     private EditText passwordEditText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +26,31 @@ public class LoginActivity extends AppCompatActivity {
 
         usernameEditText = findViewById(R.id.username);
         passwordEditText = findViewById(R.id.password);
+        passwordToggleImageView = findViewById(R.id.passwordToggleImageView);
+        passwordImageView = findViewById(R.id.passwordImageView);
 
-        Button loginButton = findViewById(R.id.login);
+        passwordToggleImageView.setOnClickListener(v->{
+            passwordToggleImageView.setVisibility(View.GONE);
+            passwordImageView.setVisibility(View.VISIBLE);
+            if (passwordEditText.getTransformationMethod() instanceof PasswordTransformationMethod) {
+                passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            } else {
+                passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+            passwordEditText.setSelection(passwordEditText.getText().length());
+        });
+        passwordImageView.setOnClickListener(v->{
+            passwordImageView.setVisibility(View.GONE);
+            passwordToggleImageView.setVisibility(View.VISIBLE);
+            if (passwordEditText.getTransformationMethod() instanceof PasswordTransformationMethod) {
+                passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            } else {
+                passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+            passwordEditText.setSelection(passwordEditText.getText().length());
+        });
+
+        loginButton = findViewById(R.id.login);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
