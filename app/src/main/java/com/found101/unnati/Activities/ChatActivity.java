c293099ae43ad56c3ed3e73d12f72617bb6b4a98
Vivek <vnjvibhash@gmail.com>
2023-03-19 13:02:15 +0530

    Chat Activity

---
+++ app/src/main/java/com/found101/unnati/Activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.found101.unnati.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.found101.unnati.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
