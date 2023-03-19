1e956bf271e017e87914da3dd9d2eb3b4eac7d3a
Vivek <vnjvibhash@gmail.com>
2023-03-19 14:24:10 +0530

    Message Adapter is Initialized

--- app/src/main/java/com/found101/unnati/Adapters/MessageAdapter.java
+++ app/src/main/java/com/found101/unnati/Adapters/MessageAdapter.java
@@ -1,4 +1,35 @@
 package com.found101.unnati.Adapters;
 
-public class MessageAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.found101.unnati.R;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MyViewHolder>{
+    @NonNull
+    @Override
+    public MessageAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_item, parent, false);
+        return new MessageAdapter.MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
