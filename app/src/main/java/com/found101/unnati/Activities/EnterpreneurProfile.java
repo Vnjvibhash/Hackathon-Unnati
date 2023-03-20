c830b9c84c05ff32893b83d662cb8cce079c5d8f
Amit Sarkar <amitskr6@gmail.com>
2023-03-20 19:55:53 +0530

    Profile for Investor and Enterprenaur created

---
+++ app/src/main/java/com/found101/unnati/Activities/EnterpreneurProfile.java
@@ -0,0 +1,16 @@
+package com.found101.unnati.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.found101.unnati.R;
+
+public class EnterpreneurProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enterprenaur_profile);
+    }
+}
---
+++ app/src/main/java/com/found101/unnati/Activities/InvestorProfile.java
@@ -0,0 +1,16 @@
+package com.found101.unnati.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.found101.unnati.R;
+
+public class InvestorProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_investor_profile);
+    }
+}
